38d807a92e32e9204ca98a21b56aebf422a1fa85
ScumLordMike <74513436+ScumLordMike@users.noreply.github.com>
2020-11-24 18:44:11 -0500

    Moar Beginner Files
    Hi

--- Java Project COP4331/src/temp/Cart.java
+++ Java Project COP4331/src/temp/Cart.java
@@ -6,4 +6,12 @@ import java.util.*;
  */
 public class Cart {
     
+    private String userID;
+    private ArrayList<Product> productList;
+    private double total;
+    
+    public Cart(String userID){
+        
+    }
+    
 }
