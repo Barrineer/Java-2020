732bb1d3d38b9a539f82ad5a1aeef856ba193f07
joeymucciaccio <69178795+joeymucciaccio@users.noreply.github.com>
2020-11-24 19:03:50 -0500

    Inventory
    Updated Inventory

--- Java Project COP4331/src/temp/Inventory.java
+++ Java Project COP4331/src/temp/Inventory.java
@@ -1,14 +1,46 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package temp;
+
+import java.util.*;
 
 /**
  *
- * @author mikey
+ * @author Joseph
  */
 public class Inventory {
+    
+    private String userID;
+    private double revenue;
+    private double sales;
+    private double profits;
+    
+    private ArrayList<Product> sellerList;
+    
+    public Inventory(String userID){
+        this.userID = userID;
+        this.sellerList = new ArrayList<Product>();
+        this.revenue = 0;
+        this.sales = 0;
+        this.profits = 0;
+    }
+    
+    public void addProduct(){
+        
+    }
+    
+    public void removeProduct(){
+        
+    }
+    
+    public double calculateRevenue(double revenue){
+        
+    }
+    
+    public double calculateSales(double sales){
+        
+    }
+    
+    public double calculateProfits(double profits){
+        
+    }
     
 }
